fc1080fee0a10a8a1b050495f4b391082d725392
purgithub <kamblepurshottam9@gmail.com>
2020-08-09 01:23:31 +0530

    User Complete purchase History

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 .then(()=> console.log("DB connected"))
 
 
-//middlewares
-
+//middlewares npm 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const { Order } = require("../models/order");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.userById = (req, res, next, id) => {
     User.findById(id).exec((err, user) => {
@@ -36,30 +38,46 @@ exports.update = (req, res) => {
     );
 };
 
-exports.addOrderToUserHistory = (req, res, next) =>{
+exports.addOrderToUserHistory = (req, res, next) => {
+    let history = [];
 
-    let history=[]
-
-    req.body.order.products.forEach((item) => {
+    req.body.order.products.forEach(item => {
         history.push({
-            _id:item._id,
-            name:item.name,
-            description:item.description,
-            quantity:item.count,
+            _id: item._id,
+            name: item.name,
+            description: item.description,
+            category: item.category,
+            quantity: item.count,
             transaction_id: req.body.order.transaction_id,
             amount: req.body.order.amount
         });
-    })
+    });
 
-    User.findOneAndUpdate({_id: req.profile._id},{$push :{history:history}},{new: true} ,(err, result) => {
-        if(err){
-            return res.status(400).json({
-                error: "Could not update user purchase history."
-            })
-           
-
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $push: { history: history } },
+        { new: true },
+        (error, data) => {
+            if (error) {
+                return res.status(400).json({
+                    error: "Could not update user purchase history"
+                });
+            }
+            next();
         }
-        next();
-    })
-}
+    );
+};
 
+exports.purchaseHistory = (req, res) => {
+    Order.find({ user: req.profile._id })
+        .populate("user", "_id name")
+        .sort("-created")
+        .exec((err, orders) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(orders);
+        });
+};
